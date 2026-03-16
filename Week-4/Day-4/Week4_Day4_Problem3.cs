ee7a944c2363a329c1bf2084734f9d8112e58f44
Gunta Divya <divyagunta74@gmail.com>
2026-03-16 07:21:38 +0530

    .

---
+++ Week-4/Day-4/Week4_Day4_Problem3.cs
@@ -0,0 +1,140 @@
+﻿/*using Week_4_Day_4_Problem_3_Hands_on_Gunta_Divya;
+Scenario:
+A company wants to maintain employee records dynamically using a Linked List structure.
+Requirements:
+-Create Node structure with employee ID and name.
+- Implement insertion at beginning and end.
+- Implement deletion by employee ID.
+- Traverse and display employee list.
+Technical Constraints:
+-Must implement singly linked list.
+- No use of built-in list structures.
+- Proper memory handling and pointer updates.
+Sample Input:
+Insert: (101, John), (102, Sara), (103, Mike)
+Delete: 102
+Sample Output:
+Employee List After Deletion:
+101 - John
+103 – Mike
+Expectations:
+-Correct node linking.
+-Efficient traversal logic.
+-Clean insertion and deletion operations.
+Learning Outcome:
+-Understand linked list structure.
+- Perform insertion and deletion operations.
+- Learn dynamic data structure behavior.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_4_Day_4_Problem_3
+{
+    // here I am creating the node with EmpID,Name,Next properties
+    class Node
+    {
+        public int EmpID;
+        public string Name;
+        public Node Next;
+        //Here I am creating a Constructor with Node.
+        public Node(int id, string name)
+        {
+            EmpID = id;
+            Name = name;
+            Next = null;
+        }
+    }
+
+    class LinkedList
+    {
+        Node head = null;
+
+        // Insert at beginning
+        public void InsertAtBeginning(int id, string name)
+        {
+            Node newNode = new Node(id, name);
+            newNode.Next = head;
+            head = newNode;
+        }
+
+        // Insert at end
+        public void InsertAtEnd(int id, string name)
+        {
+            Node newNode = new Node(id, name);
+
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            Node temp = head;
+
+            while (temp.Next != null)
+            {
+                temp = temp.Next;
+            }
+
+            temp.Next = newNode;
+        }
+
+        // Here I am deleting the EmpId 
+        public void Delete(int id)
+        {
+            if (head == null)
+                return;
+
+            if (head.EmpID == id)
+            {
+                head = head.Next;
+                return;
+            }
+
+            Node temp = head;
+
+            while (temp.Next != null && temp.Next.EmpID != id)
+            {
+                temp = temp.Next;
+            }
+
+            if (temp.Next != null)
+            {
+                temp.Next = temp.Next.Next;
+            }
+        }
+
+        // Display employees
+        public void Display()
+        {
+            Node temp = head;
+
+            while (temp != null)
+            {
+                Console.WriteLine(temp.EmpID + " - " + temp.Name);
+                temp = temp.Next;
+            }
+        }
+    }
+    internal class Week4_Day4_Problem3
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+
+            // Sample Input
+            list.InsertAtEnd(2, "Divya");
+            list.InsertAtEnd(3, "Jaya sri");
+            list.InsertAtEnd(4,"Ambika");
+
+            list.Delete(4);
+
+            Console.WriteLine("Employee List After Deletion:");
+            list.Display();
+        }
+    }
+}
---
+++ Week-4/Day-4/Week_4_Day_4_p1.cs
@@ -0,0 +1,92 @@
+﻿/*Scenario:
+A training institute wants to analyze student scores stored in an array. The system should calculate total marks, average, highest score, and count of students scoring above a threshold.
+Requirements:
+- Store student marks in an array.
+- Use array methods (push, map, filter, reduce) for processing.
+- Store subject-wise highest marks using a Map (key-value pair).
+- Display total, average, and filtered results.
+Technical Constraints:
+- Must use array indexing and iteration.
+- Use reduce() for total calculation.
+- Use filter() for threshold-based filtering.
+- Use Map or Dictionary for subject-highest mapping.
+Sample Input:
+Marks: [78, 85, 90, 67, 88]
+Threshold: 80
+Sample Output:
+Total Marks: 408
+Average Marks: 81.6
+Students above 80: 3
+Highest Score: 90
+Expectations:
+- Clean and modular implementation.
+- Proper use of array methods.
+- Efficient iteration logic.
+Learning Outcome:
+- Understand array manipulation.
+- Use Map for key-value storage.
+- Apply functional programming methods.
+*/
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Week_4_Day_4
+{
+    internal class Week_4_Day_4_p1
+    {
+        static void Main(string[] args)
+        {
+            //Here I am storing the marks in an array.
+            int[] marks = { 78, 85, 90, 67, 88 };
+            int threshold = 80;
+            //here I am using the aggregate method to aggregation.
+            int Totalmarks = marks.Aggregate((sum, mark) => sum + mark);
+            double Averagemarks = (double)Totalmarks / marks.Length;
+            int Highestscore = marks.Max();
+            var AboveThreshold = marks.Where(mark => mark > threshold);
+            Console.WriteLine("Total Marks" + Totalmarks);
+            Console.WriteLine("Average Marks" + Averagemarks);
+            Console.WriteLine("Student above" + threshold + ":" + AboveThreshold.Count());
+            Console.WriteLine("Highest Score" + " " + Highestscore);
+        }
+        //Here I am using the dictionary to store subject wise highest marks.
+        static Dictionary<string, int> GetSubjectHighestMarks()
+        {
+            Dictionary<string, int[]> subjectMarks = new Dictionary<string, int[]>
+            {
+                { "Math", new int[] {78,85,90,67,88} },
+                { "Physics", new int[] {80,70,95,88,84} },
+                { "Chemistry", new int[] {60,75,89,92,77} }
+            };
+
+            Dictionary<string, int> subjectHighest = new Dictionary<string, int>();
+
+            foreach (var subject in subjectMarks)
+            {
+                // Aggregate used to find highest mark
+                int highest = subject.Value.Aggregate((MAX, m) => m > MAX ? m : MAX);
+
+                subjectHighest.Add(subject.Key, highest);
+            }
+
+            return subjectHighest;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
